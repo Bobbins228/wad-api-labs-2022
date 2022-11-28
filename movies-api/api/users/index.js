c9074705da8d623b95609cc087246bdc2db16d88
Bobbins228 <markcampbell114@gmail.com>
2022-11-28 15:32:20 +0000

    Topic 11 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -19,8 +19,14 @@ router.post('/',asyncHandler( async (req, res, next) => {
       return next();
     }
     if (req.query.action === 'register') {
-      await User.create(req.body);
-      res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      if (req.body.password.match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$")){
+        res.status(401).json({code: 401,msg: 'Registration failed. Bad password'});
+      }
+      else{
+        await User.create(req.body);
+        res.status(201).json({code: 201, msg: 'Successful created new user.'});
+      }
+      
     } else {
       const user = await User.findByUserName(req.body.username);
         if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
@@ -57,6 +63,9 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const userName = req.params.userName;
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
+    if (user.favourites.includes(movie._id)) {
+        res.status(404).json({ code: 404, msg: 'Duplicate movie found' });
+    }
     await user.favourites.push(movie._id);
     await user.save(); 
     res.status(201).json(user); 
